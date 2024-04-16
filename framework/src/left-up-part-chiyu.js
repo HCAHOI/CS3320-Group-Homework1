b7076206e3d6b3db90bc241e172a7d753b7ff60f
HCAHOI <hcahoi11@sjtu.edu.cn>
2024-04-16 13:26:17 +0800

    add basic element for borrowTop4

--- framework/src/left-up-part-chiyu.js
+++ framework/src/left-up-part-chiyu.js
@@ -4,16 +4,32 @@ document.addEventListener('DOMContentLoaded', function () {
   let departmentBox = document.querySelector('#department-distribution .data-box')
   let borrowTopBox = document.querySelector('#borrow-top .data-box')
 
-  let testDiv1 = document.createElement('div');
-  testDiv1.textContent = 'test';
-  testDiv1.style.color = 'red';
+  let viewSelector = document.createElement('div');
+  viewSelector.id = 'view-selector';
+  let weekViewButton = document.createElement('button');
+  weekViewButton.id = 'week-view';
+  weekViewButton.innerText = '周视图';
+  let monthViewButton = document.createElement('button');
+  monthViewButton.id = 'month-view';
+  monthViewButton.innerText = '月视图';
+  let quarterViewButton = document.createElement('button');
+  quarterViewButton.id = 'quarter-view';
+  quarterViewButton.innerText = '季视图';
+  let yearViewButton = document.createElement('button');
+  yearViewButton.id = 'year-view';
+  yearViewButton.innerText = '年视图';
+  viewSelector.appendChild(weekViewButton);
+  viewSelector.appendChild(monthViewButton);
+  viewSelector.appendChild(quarterViewButton);
+  viewSelector.appendChild(yearViewButton);
 
-  let testDiv2 = testDiv1.cloneNode(true);
-  let testDiv3 = testDiv1.cloneNode(true);
+  let viewTimeSpan = document.createElement('div');
+  viewTimeSpan.id = 'view-time-span';
 
-  readerBox.appendChild(testDiv1);
-  departmentBox.appendChild(testDiv2);
-  borrowTopBox.appendChild(testDiv3);
+  let viewTop4 = document.createElement('div');
+  viewTop4.id = 'view-top-4';
 
-  console.log(readerBox, departmentBox, borrowTopBox);
+  borrowTopBox.appendChild(viewSelector);
+  borrowTopBox.appendChild(viewTimeSpan);
+  borrowTopBox.appendChild(viewTop4);
 });
