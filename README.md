00c18a2f2b24cb5d5ad093b57cd86c0a34f9e8f6
HCAHOI <hcahoi11@sjtu.edu.cn>
2024-05-05 11:28:29 +0800

    final commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# CS3320-Group-Homework1
-
-Please put the data into `data` directory by yourself.
---
+++ code/src/main.js
@@ -0,0 +1,11 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import * as Chiyu from './left-up-part-chiyu.js'
+import * as Haoyuan from './lhy.js'
+import './dyz.js'
+import './csc.js'
+createApp(App).mount('#app')
+
